11113dc9c78d7f5707ad4ed41957e2eb7030b0b5
Abdallah <abdallahchaar90@gmail.comh>
2021-01-09 12:47:25 +0100

    main.js utveckling

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
+/* global console, alert, prompt */
+
+/*
 var myInput = document.getElementById("input"),
 
     myDiv = document.getElementById("div"),
 
     myButton = document.getElementById("button");
 
-    // Element.Event = function () { // Code}
-
-myButton.onclick = function () {
-    /*
-    console.log(myInput);
-    console.log(myDiv);
-    */
-    /*function calcUsd() {*/
+    myButton.onclick = function () {
 
     "use strict";
-    myDiv.innerHTML = myInput.value * 3.75;
 
-};
+    myDiv.innerHTML = myInput.value * 3.75;
+    };
+*/
+
+// Element.Event = function () { // Code}
+
+
+/*
+console.log(myInput);
+console.log(myDiv);
+*/
+/*function calcUsd() {*/
+
+
+
+var myInput = document.getElementById("input"),
+
+    myDiv = document.getElementById("div"),
+
+    myButton = document.getElementById("button");
+
+    myButton.onclick = function () {
+
+    "use strict";
+
+    myDiv.innerHTML = myInput.value * 3.75;
